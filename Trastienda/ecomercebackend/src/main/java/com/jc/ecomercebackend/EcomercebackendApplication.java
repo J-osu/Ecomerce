246f87237796bfb6de9c5aec99e46ea81793464a
J-osu <josuedelacr96@gmail.com>
2025-03-14 16:18:14 +0100

    Bases puestas para el back y el front.

---
+++ Trastienda/ecomercebackend/src/main/java/com/jc/ecomercebackend/EcomercebackendApplication.java
@@ -0,0 +1,13 @@
+package com.jc.ecomercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomercebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomercebackendApplication.class, args);
+	}
+
+}
---
+++ Trastienda/ecomercebackend/src/test/java/com/jc/ecomercebackend/EcomercebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.ecomercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomercebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
